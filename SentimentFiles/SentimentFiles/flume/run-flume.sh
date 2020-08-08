4874d3e1f87f04a2a1af8229eae3a1b7d8a5558c
Anubhav <anubhav6864@gmail.com>
2020-08-08 11:50:01 +0530

    deleted flume files

--- SentimentFiles/SentimentFiles/flume/run-flume.sh
+++
@@ -1,2 +0,0 @@
-echo "flume-ng agent -n TwitterAgent -c ~/sentiment/flume/conf -f ~/sentiment/flume/twitter.conf &"
-flume-ng agent -n TwitterAgent -c ~/sentiment/flume/conf -f ~/sentiment/flume/twitter.conf & 
